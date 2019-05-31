2685949b2632b15904b370f6f036b96651f34c1f
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-05-31 10:53:43 -0600

    used moment to add next train times

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -18,10 +18,10 @@
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
-  var trainName = $("#train-name-input").val().trim();
+  var trainName   = $("#train-name-input").val().trim();
   var destination = $("#destination-input").val().trim();
-  var trainTime = moment($("#train-time-input").val().trim(), "HH:mm").format("HH:mm");
-  var frequency = $("#frequency-input").val().trim();
+  var trainTime   = moment($("#train-time-input").val().trim(), "HH:mm").format("HH:mm");
+  var frequency   = $("#frequency-input").val().trim();
 
   var newTrain = {
     trainName: trainName,
@@ -32,6 +32,46 @@ $("#add-train-btn").on("click", function(event) {
 
 database.ref().push(newTrain);
 
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#train-time-input").val("");
+$("#frequency-input").val("");
+
+});
+
+database.ref().on("child_added", function(snapshot){
+
+  var trainName   = snapshot.val().trainName;
+  var destination = snapshot.val().destination;
+  var trainTime   = snapshot.val().trainTime;
+  var frequency   = snapshot.val().frequency;
+
+  //calculate the next train time
+  var firstTrainTime = moment(trainTime, "HH:mm").subtract(1, "years");
+  console.log(firstTrainTime);
+
+  var currentTime = moment();
+  
+  var timeBetween = currentTime.diff(moment(firstTrainTime), "minutes");
+  console.log(timeBetween);
+    
+  var minutesRemainder = timeBetween % frequency;
+  console.log(minutesRemainder);
+
+  var minutesUntilNextTrain = frequency - minutesRemainder;
+
+  var nextTrain = moment().add(minutesUntilNextTrain, "minutes").format("hh:mm A");
+
+  var row = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(nextTrain),
+    $("<td>").text(minutesUntilNextTrain)
+  );
+
+  $("#time-table").append(row);
+
 });
   //Store variables and handle push
 
@@ -39,6 +79,3 @@ database.ref().push(newTrain);
 
   //Grab onchild events and store
 
-  //Display to HTML
-
-  
